5268e088c87f3a1e54187716ddf027f2eaafefff
semenov <semenov>
2025-05-25 11:36:35 +0400

    2 Ядро игры

---
+++ src/core/damageFormula.ts
@@ -0,0 +1,22 @@
+import { Character } from "@src/character/character";
+
+// Формула рассчета урона
+export function damageFormula(character: Character, character2: Character) {
+    //Урон персонажа: Обычный урон * Скорость атаки
+    const damageCharacter = character.damage.getValue() * character.speed.getValue();
+    //Урон по персонажу: Урон персонажа / сопративлении брони
+    const datageInCharacter = damageCharacter / (character2.arror.getValue() / 100);
+    return datageInCharacter;
+}
+
+// нанести урон персонажу
+export function damageCharacter(character: Character, damage: number) {
+    // пока есть барьер урон по здоровью не будет, весь сверхУрон полностью поглащается барьером.
+    const barrierValue = character.barrier.getValue();
+    if (barrierValue != 0) {
+        character.barrier.setValue(barrierValue - damage);
+        return;
+    }
+    const healthValue = character.health.getValue();
+    character.health.setValue(healthValue - damage);
+}
---
+++ src/core/games.ts
@@ -0,0 +1,38 @@
+import { Character } from "@src/character/character";
+import { damageCharacter, damageFormula } from "./damageFormula";
+
+export function games(character: Character, character2: Character) {
+    let xod = 0;
+    let characterXod = 1; // определяет чей ход наносить урон.
+
+    // Бесконечный цикл игры выходит когда у 1 персонажа закончится хп.
+    while (true) {
+        xod++;
+        if (character.health.getValue() == 0 || character2.health.getValue() == 0) {
+            break;
+        }
+
+        // Подумать как улучшить!
+        if (characterXod == 1) {
+            const damageCount = damageFormula(character, character2);
+            damageCharacter(character2, damageCount);
+            console.log(`Персонаж 1 нанес урон ${damageCount}, персонажа 2 осталось барьера ${character2.barrier.getValue()}, ХП ${character2.health.getValue()}`);
+            characterXod = 2;
+            continue;
+        }
+
+
+        if (characterXod == 2) {
+            const damageCount = damageFormula(character2, character);
+            damageCharacter(character, damageCount);
+            console.log(`Персонаж 2 нанес урон ${damageCount}, персонажа 1 осталось барьера ${character.barrier.getValue()}, ХП ${character.health.getValue()}`);
+            characterXod = 1;
+            continue;
+        }
+        // Подумать как улучшить!
+        break;
+    }
+    console.log("Конец игры", xod);
+    console.log(character.health.getValue(), character2.health.getValue());
+
+}
