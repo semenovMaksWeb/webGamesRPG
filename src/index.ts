4c784d0a50c8487c1c0d1cc6d8317acb6196c261
semenov <semenov>
2025-05-25 11:39:09 +0400

    тестовые запуски кода

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { Character } from "./character/character.js";
+import { games } from "./core/games.js";
 import { generatorAttribure } from "./core/generatorAttribure.js";
 
-const character1 = {
+const character1Config = {
     health: 80,
     arror: 20,
-    barrier: 60
+    barrier: 60,
+    damage: 10,
+    speed: 1,
 }
 
-const characterObjectAttribute = generatorAttribure(character1);
-const character = new Character(characterObjectAttribute);
-console.log(character);
+const character2Config = {
+    health: 100,
+    arror: 50,
+    barrier: 100,
+    damage: 4,
+    speed: 0.4,
+}
+
+const characterObjectAttribute1 = generatorAttribure(character1Config);
+const characterObjectAttribute2 = generatorAttribure(character2Config);
+const character1 = new Character("Тестовый персонаж", characterObjectAttribute1);
+const character2 = new Character("Тестовый персонаж2", characterObjectAttribute2);
+console.log(character1, character2);
+games(character1, character2);
