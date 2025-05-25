9f8c116010eb25965bae34593a70511fb72405ca
semenov <semenov>
2025-05-25 20:48:38 +0400

    тестовые запуски кода

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,8 @@
-import { Character } from "./character/character.js";
-import { games } from "./core/games.js";
-import { generatorAttribure } from "./core/generatorAttribure.js";
+import { games } from "./core/games";
 
-const character1Config = {
-    health: 80,
-    arror: 20,
-    barrier: 60,
-    damage: 10,
-    speed: 1,
-}
+import { FellerСharacter } from "@src/character/FellerСharacter"
 
-const character2Config = {
-    health: 100,
-    arror: 50,
-    barrier: 100,
-    damage: 4,
-    speed: 0.4,
-}
+const character1 = new FellerСharacter();
+const character2 = new FellerСharacter();
 
-const characterObjectAttribute1 = generatorAttribure(character1Config);
-const characterObjectAttribute2 = generatorAttribure(character2Config);
-const character1 = new Character("Тестовый персонаж", characterObjectAttribute1);
-const character2 = new Character("Тестовый персонаж2", characterObjectAttribute2);
-console.log(character1, character2);
 games(character1, character2);
