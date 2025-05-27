eee55a1ea2bce31fe039e8aa36f9095558d411a2
semenov <semenov>
2025-05-27 20:25:04 +0400

    тестовый запуск

--- src/core/games/games.ts
+++ src/core/games/games.ts
@@ -37,39 +37,3 @@ export function gamesPlayer(player1: Player, player2: Player) {
     console.log("Конец игры", xod);
     console.log(player1.characterPlayer.health.getValue(), player2.characterPlayer.health.getValue());
 }
-
-export function games(character: Character, character2: Character) {
-    let xod = 0;
-    let characterXod = 1; // определяет чей ход наносить урон.
-
-    // Бесконечный цикл игры выходит когда у 1 персонажа закончится хп.
-    while (true) {
-        xod++;
-        if (character.health.getValue() == 0 || character2.health.getValue() == 0) {
-            break;
-        }
-
-        // Подумать как улучшить!
-        if (characterXod == 1) {
-            const damageCount = damageFormula(character, character2);
-            damageCharacter(character2, damageCount);
-            console.log(`Персонаж 1 нанес урон ${damageCount}, персонажа 2 осталось барьера ${character2.barrier.getValue()}, ХП ${character2.health.getValue()}`);
-            characterXod = 2;
-            continue;
-        }
-
-
-        if (characterXod == 2) {
-            const damageCount = damageFormula(character2, character);
-            damageCharacter(character, damageCount);
-            console.log(`Персонаж 2 нанес урон ${damageCount}, персонажа 1 осталось барьера ${character.barrier.getValue()}, ХП ${character.health.getValue()}`);
-            characterXod = 1;
-            continue;
-        }
-        // Подумать как улучшить!
-        break;
-    }
-    console.log("Конец игры", xod);
-    console.log(character.health.getValue(), character2.health.getValue());
-
-}
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import config2 from "@src/test/accountAdmin2.json"
 
 const account1 = new Account(config1);
 const account2 = new Account(config2);
-console.log(account1.config);
 
 const player1 = account1.configConvertToPlayer(0, 0);
-const player2 =  account2.configConvertToPlayer(0, 0);
+const player2 = account2.configConvertToPlayer(0, 0);
 gamesPlayer(player1, player2);
