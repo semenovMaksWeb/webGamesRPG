dc9617a593e1effa04df2e7d8339b9c8b21b7ae5
semenov <semenov>
2025-05-30 17:31:24 +0400

    G1 - настройка среды и Electron

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { gamesPlayer } from "./core/games/gamesFun";
 import { Account } from "./core/account/account";
 import config1 from "@src/test/accountAdmin.json"
 import config2 from "@src/test/accountAdmin2.json"
+import { AccountConfig } from "./core/account/AccountConfig";
 
-const account1 = new Account(config1);
-const account2 = new Account(config2);
+const account1 = new Account(config1 as AccountConfig);
+const account2 = new Account(config2 as AccountConfig);
 
 const player1 = account1.configConvertToPlayer(0, 0);
 const player2 = account2.configConvertToPlayer(0, 0);
