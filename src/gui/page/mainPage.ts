6f1fc794d15667b1d9fa901c63f66a2e06511ebe
semenov <semenov>
2025-05-31 14:04:33 +0400

    G3 - логика переключение содержимого

---
+++ src/gui/page/mainPage.ts
@@ -0,0 +1,5 @@
+import { dom } from "@src/gui/dom/dom";
+
+export function mainPage() {
+    dom.create.menuMain();
+}
--- src/gui/script.ts
+++ src/gui/script.ts
@@ -1,13 +1,16 @@
 import { indexedDBService } from "@src/gui/IndexedDB/IndexedDB";
 import { formUserNameForm } from "@src/gui/page/formUserNameForm";
 import "@src/gui/style/main.css"
+import { mainPage } from "@src/gui/page/mainPage";
 
 (async () => {
     const getUser = await indexedDBService.getUser();
 
     if (!getUser) {
         formUserNameForm();
+        return;
     }
+    mainPage();
 
 })();
 
