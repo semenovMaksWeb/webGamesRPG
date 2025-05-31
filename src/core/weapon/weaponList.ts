0efd0c91ddd993425109b564f7690f01d33946ff
semenov <semenov>
2025-05-31 12:41:01 +0400

    Описание списка оружия

--- src/core/weapon/weaponList.ts
+++ src/core/weapon/weaponList.ts
@@ -1,7 +1,12 @@
 export enum weaponList {
+    // Лук новичка
     LookNoviceWeapon = "LookNoviceWeapon",
+    // Лук мощности
     LookPowerWeapon = "LookPowerWeapon",
+    // Лук вампира
     LookVampireWeapon = "LookVampireWeapon",
+    // Топор
     NearAxWeapon = "NearAxWeapon",
+    // Нож
     NearKnifeWeapon = "NearKnifeWeapon",
 }
