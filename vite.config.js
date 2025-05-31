bbc2770a29c5f083188f6c7b81c655fc29bec023
semenov <semenov>
2025-05-31 09:31:07 +0400

    G1 - настройка среды и Electron

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
+import * as path from 'path';
+
+export default defineConfig({
+    root: "src/gui/",
+    plugins: [tsconfigPaths()],
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+        }
+    }
+})
