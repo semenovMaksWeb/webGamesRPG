e49bf3d57b4aec72c0b41c889d493dd192ce9d19
semenov <semenov>
2025-05-31 19:23:50 +0400

    Я20 - мелкие правки (сущность описание монет)

--- src/core/coins/coinsList.ts
+++ src/core/coins/coinsList.ts
@@ -2,3 +2,14 @@ export enum coinsList {
     wooden = "wooden", // Деревянные
     battle = "battle", // Боевые
 }
+
+export const coinsListInfo: any = {
+    wooden: {
+        name: "Деревянные",
+        icons: ""
+    },
+    battle: {
+        name: "Боевые",
+        icons: ""
+    }
+}
