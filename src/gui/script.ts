f3f04a2bc7be9df189449d7daf3b21b7d78c619a
semenov <semenov>
2025-05-31 10:11:07 +0400

    G3 - логика переключение содержимого

--- src/gui/script.ts
+++ src/gui/script.ts
@@ -1,5 +1,6 @@
 import { indexedDBService } from "@src/gui/IndexedDB/IndexedDB";
 import { formUserNameForm } from "@src/gui/page/formUserNameForm";
+import "@src/gui/style/main.css"
 
 (async () => {
     const getUser = await indexedDBService.getUser();
