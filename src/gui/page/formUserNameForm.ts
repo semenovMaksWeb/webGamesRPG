4d21da94ba0795cb496c0358b78cc70a458a8649
semenov <semenov>
2025-05-31 10:11:23 +0400

    G3 - логика переключение содержимого

--- src/gui/page/formUserNameForm.ts
+++ src/gui/page/formUserNameForm.ts
@@ -1,16 +1,24 @@
 import { dom } from "@src/gui/dom/dom";
+import "@src/gui/style/formUserNameForm.css"
 
 export function formUserNameForm() {
     const APP = document.querySelector("#APP") as Element;
-    
+
     const formUserNameForm = document.createElement("form");
     formUserNameForm.classList.add("form");
     formUserNameForm.classList.add("formUserNameForm");
 
-    const input = dom.input();  
 
-    const button = dom.button("Подвердить имя")
+    const label = document.createElement("label");
+    formUserNameForm.classList.add("label");
+    label.innerHTML = "Введите имя";
+    label.setAttribute("for", "name");
 
+    const input = dom.input("name");
+
+    const button = dom.button("Подвердить имя");
+
+    formUserNameForm.append(label);
     formUserNameForm.append(input);
     formUserNameForm.append(button);
 
