eca63cf7770c0ccd2c177457c59f8ffe0dc3dea4
semenov <semenov>
2025-05-31 10:11:15 +0400

    G4 - dom

--- src/gui/dom/createInput.ts
+++ src/gui/dom/createInput.ts
@@ -1,5 +1,7 @@
-export function createInput() {
+export function createInput(idAttribute: string) {
     const input = document.createElement("input");
     input.classList.add("input");
+    input.setAttribute("id", idAttribute);
+    input.setAttribute("autocomplete", "off");
     return input;
 }
